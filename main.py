2647e2af95effdc6f99882cf1c72b9223fd9b649
s-dub <ckl15076@nitech.jp>
2019-09-29 11:28:20 +0900

    connect watson assistant and slack

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 import os
 import slack
+from ibm_watson import AssistantV2
 
 
 @slack.RTMClient.run_on(event='message')
-def say_hello(**payload):
+def react(**payload):
     data = payload['data']
     web_client = payload['web_client']
     if 'client_msg_id' in data:
+        response = service.message(
+            assistant_id=os.environ["ASSISTANT_ID"],
+            session_id=session_id,
+            input={'message_type': 'text',
+                   'text': data['text']}
+        ).get_result()
+        print(response)
+
         channel_id = data['channel']
         web_client.chat_postMessage(
             channel=channel_id,
-            text="ringo",
+            text=response['output']['generic'][0]['text'],
         )
 
 
-slack_token = os.environ["SLACK_API_TOKEN"]
+service = AssistantV2(
+    iam_apikey=os.environ["WATSON_API_KEY"],
+    version='2019-02-28',
+    url='https://gateway.watsonplatform.net/assistant/api'
+)
+session_id = service.create_session(
+    assistant_id=os.environ["ASSISTANT_ID"]
+).get_result()['session_id']
+
+slack_token = os.environ["SLACK_BOT_TOKEN"]
 rtm_client = slack.RTMClient(token=slack_token)
 rtm_client.start()
